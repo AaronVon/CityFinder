47bf8d11b11867650cba53598d10236730999ebb
Aaron <pioneervon@gmail.com>
2015-12-22 11:10:25 +0800

    Handling result between mainactivity and Finder activitivity
    Signed-off-by: Aaron <pioneervon@gmail.com>

---
+++ app/src/main/java/com/pioneer/aaron/cityfinder/Finder/Finder.java
@@ -0,0 +1,14 @@
+package com.pioneer.aaron.cityfinder.Finder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Aaron on 12/22/15.
+ */
+public class Finder extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/pioneer/aaron/cityfinder/MainActivity.java
+++ app/src/main/java/com/pioneer/aaron/cityfinder/MainActivity.java
@@ -1,13 +1,44 @@
 package com.pioneer.aaron.cityfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.pioneer.aaron.cityfinder.Finder.Finder;
 
 public class MainActivity extends AppCompatActivity {
+    TextView location_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        location_text = (TextView) findViewById(R.id.home_location_text);
+        location_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Finder.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (null != data) {
+            switch (resultCode) {
+                case 2:
+                    location_text.setText(data.getStringExtra("city"));
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
