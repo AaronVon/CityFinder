fe462c5562d74a7f01ea5dddfefde7af7531d2c9
Aaron <pioneervon@gmail.com>
2015-12-27 23:14:50 +0800

    Add butterknife
    Signed-off-by: Aaron <pioneervon@gmail.com>

--- app/src/main/java/com/pioneer/aaron/cityfinder/MainActivity.java
+++ app/src/main/java/com/pioneer/aaron/cityfinder/MainActivity.java
@@ -1,18 +1,46 @@
 package com.pioneer.aaron.cityfinder;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.pioneer.aaron.cityfinder.activities.PinyinSearch;
 import com.pioneer.aaron.cityfinder.finder.Finder;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.home_location_text)
     TextView location_text;
-    ImageButton locationButton;
+
+    @OnClick({R.id.btn_locate, R.id.home_location_text})
+    public void onClickView(View view) {
+        switch (view.getId()) {
+            case R.id.btn_locate:
+                Intent intent = new Intent(this, Finder.class);
+                startActivityForResult(intent, 1);
+                break;
+            case R.id.home_location_text:
+                Snackbar.make(location_text, "You're at " + location_text.getText(), Snackbar.LENGTH_SHORT)
+                        .setAction("OK", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        })
+                        .show();
+                
+                break;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,16 +49,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        location_text = (TextView) findViewById(R.id.home_location_text);
-        locationButton = (ImageButton) findViewById(R.id.btn_locate);
-        locationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Finder.class);
-                startActivityForResult(intent, 1);
-            }
-        });
-
+        ButterKnife.bind(this);
     }
 
     @Override
